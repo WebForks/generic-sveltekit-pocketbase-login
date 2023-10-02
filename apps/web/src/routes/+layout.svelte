49d19217de956a56470f428a91f882ac845fbec8
WebForks <35718739+a3eon-1@users.noreply.github.com>
2023-10-01 22:46:52 -0700

    inital comitt

---
+++ apps/web/src/routes/+layout.svelte
@@ -0,0 +1,50 @@
+<script>
+  import "../app.postcss";
+  export let data;
+</script>
+
+<div class="min-h-full">
+  <nav class="navbar bg-base-100 border-b">
+    <div class="flex-1">
+      <a href="/" class="btn btn-ghost normal-case text-xl"> OptionTracker </a>
+    </div>
+    <div class="flex-none">
+      {#if !data.user}
+        <div class="dropdown dropdown-bottom dropdown-end">
+          <button aria-label="Click" class="btn m-1" tabindex="0">Menu</button>
+          <ul
+            class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li><a href="/about" class="btn btn-primary">About</a></li>
+            <li><a href="/changelog" class="btn btn-primary">Changelog</a></li>
+            <li><a href="/login" class="btn btn-primary">Login</a></li>
+            <li><a href="/register" class="btn btn-primary">Register</a></li>
+          </ul>
+        </div>
+      {:else}
+        <div class="dropdown dropdown-bottom dropdown-end">
+          <button aria-label="Click" class="btn m-1" tabindex="0">Menu</button>
+          <ul
+            class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li><a href="/about" class="btn btn-primary">Profile</a></li>
+            <li><a href="/logsearch" class="btn btn-primary">logsearch</a></li>
+            <li><a href="/about" class="btn btn-primary">About</a></li>
+            <li><a href="/changelog" class="btn btn-primary">Changelog</a></li>
+            <li>
+              <form action="/logout" method="POST">
+                <button type="submit" class="btn btn-primary">Logout</button>
+              </form>
+            </li>
+          </ul>
+        </div>
+      {/if}
+    </div>
+  </nav>
+
+  <div class="py-10">
+    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <slot />
+    </div>
+  </div>
+</div>
